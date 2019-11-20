871ba9bd56e8e738baa35b5d95d7fa360d53dda4
yizhigou <twtwwei@gmail.com>
2019-11-20 19:16:18 +0800

    Overhaul

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('#overlay').on('click',
 function(event) {
     if (event.target.tagName === 'BUTTON' && event.target.id) {
-        document.getElementById('elder-' + /\d+/.exec(event.target.id)[0]).play();
+        document.getElementById('voice-' + /\d+/.exec(event.target.id)[0]).play();
     }
 });
